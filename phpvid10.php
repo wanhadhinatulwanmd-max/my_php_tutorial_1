9344096e1e3586c805e40fd1a9e9d5e55e75f72f
wanhadhinatulwanmd-max <wanhadhinatul.wanmd@student.gmi.edu.my>
2026-09-19 08:11:52 +0800

    Add files via upload

---
+++ phpvid10.php
@@ -0,0 +1,44 @@
+<?php
+
+//comparison booleans (true or false)
+
+//numbers
+//echo 5 < 10;
+//echo 5 > 10;
+//echo 5 == 10;
+//echo 5 != 10;
+//echo 5 <= 5;
+//echo 5 >= 5;
+
+//strings
+//echo 'doom' < 'night';
+//echo 'doom' > 'night;
+//echo 'doom' > 'Doom';
+//echo 'moon' == 'moon';
+//echo 'moon' == 'Moon';
+
+
+//loose vs strict equal comparison
+//echo 5 == '5';
+//echo 5 === '5';
+//echo 5 === 5;
+
+//echo true == "1";
+echo false == "";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP TUTO 8</title>
+    </head>
+    <body>
+
+    <h1>COMPARISON & BOOLEAN</h1>
+
+    
+    </body>
+</html>
